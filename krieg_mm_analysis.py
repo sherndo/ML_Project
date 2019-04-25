e9259749b566e45a224ff55db5412f0cb001e347
sjkrieg <44177511+sjkrieg@users.noreply.github.com>
2019-04-24 22:09:08 -0400

    Add files via upload

---
+++ krieg_mm_analysis.py
@@ -0,0 +1,73 @@
+# Parameter log for mean log loss:
+# Naive Bayes:
+#   Default parameters: 0.706633
+#
+# Logistic Regression:
+#   C=100.0: 0.550231
+#   solver='newton-cg': 0.555598
+#   Default parameters: 0.555604
+#   solver='lbfgs': 0.559913
+#   solver='sag' & max_iter=1000: 0.559996
+#   solver='newton-cg', multi_class='multinomial': 0.583119
+#
+#   tol: no significant difference
+#   penalty: no significant difference
+
+#
+# Gradient Boosting:
+#   criterion='mse', n_estimators=45: 0.560244 
+#   n_estimators=50: 0.561023
+#   default: 0.568436
+#   loss='exponential' = 0.575329
+
+#
+# XGBoost:
+#   booster='dart': 0.561123
+#   booster='gbtree': 0.563486
+#   booster='gblinear': 0.563554
+#
+#   booster='dart' w/ mod eta: no significant difference
+#   increasing gamma : bad
+#   varying max_depth : bad
+#   nothing else seemed to make a difference
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from xgboost import XGBClassifier
+from sklearn.metrics import log_loss
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
+df = pd.read_csv('tourney_matchup_results_wmirrors.csv')
+cols_to_exclude = []
+results = pd.DataFrame(columns=['Test Year','NB Acc','LR Acc', 'XGB Acc','NB Ll','LR Ll','XGB Ll'])
+x = df.iloc[:, 0:len(df.columns)-1]
+y = df.iloc[:, -1]
+
+nb = GaussianNB()
+lr = LogisticRegression(C=100)
+xgb = XGBClassifier(booster='dart')
+
+for year in x['Year'].unique():
+    x_train = x[x['Year'] != year].drop(cols_to_exclude, axis=1)
+    y_train = y.iloc[x_train.index]
+    x_test = x[x['Year'] == year].drop(cols_to_exclude, axis=1)
+    y_test = y.iloc[x_test.index]
+    
+    nb.fit(x_train, y_train)
+    lr.fit(x_train, y_train)
+    xgb.fit(x_train, y_train)
+    
+    results.loc[len(results.index)] = [year, 
+                nb.score(x_test, y_test), 
+                lr.score(x_test, y_test),
+                xgb.score(x_test, y_test),
+                log_loss(y_test, nb.predict_proba(x_test)), 
+                log_loss(y_test, lr.predict_proba(x_test)), 
+                log_loss(y_test, xgb.predict_proba(x_test)) ]
+
+results.loc[len(results.index)] = ['Avg'] + [results[col].mean() for col in results.columns if col != 'Test Year']
+results.loc[len(results.index)] = ['STD'] + [results[col].std() for col in results.columns if col != 'Test Year']
+print(results[['Test Year'] + [x for x in results.columns if x.endswith('Ll')]])
+results.to_csv('krieg_mm_results.csv', index=False)
---
+++ krieg_plot_mm_results.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from itertools import product
+import seaborn as sns
+sns.set(style="darkgrid", font_scale=1.8, rc={"lines.linewidth": 3})
+
+df = pd.read_csv('krieg_mm_results.csv')
+df_new = pd.DataFrame(columns=['Test Year','Model','Accuracy','Log Loss'])
+
+models = ['NB','LR','XGB']
+metrics = ['Acc','Ll']
+rows_to_exclude = ['Avg','STD']
+years = [x for x in df['Test Year'].unique() if x not in rows_to_exclude]
+
+for y, m in product(years, models):
+    df_new.loc[len(df_new.index)] = [y[:4], m, 
+               df.loc[df['Test Year'] == y][m + ' ' + metrics[0]].values[0],
+               df.loc[df['Test Year'] == y][m + ' ' + metrics[1]].values[0] ]
+
+#sns.relplot(data=df_new, kind='line', x='Test Year',y='Log Loss',hue='Model', style='Model', height=10, aspect=2)
+sns.catplot(data=df_new, x='Model', y='Log Loss', kind='violin', ci='sd', height=8, aspect=1.5)
