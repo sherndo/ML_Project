6ef0ffb8b9e156bd57c5d94175eb426a02662fc7
sjkrieg <44177511+sjkrieg@users.noreply.github.com>
2019-04-26 12:55:15 -0400

    Add files via upload

--- krieg_mm_analysis.py
+++ krieg_mm_analysis.py
@@ -46,10 +46,11 @@ x = df.iloc[:, 0:len(df.columns)-1]
 y = df.iloc[:, -1]
 
 nb = GaussianNB()
-lr = LogisticRegression(C=100)
+lr = LogisticRegression(penalty='l1', tol=1e-5, C=100, solver='liblinear', max_iter=500)
 xgb = XGBClassifier(booster='dart')
 
 for year in x['Year'].unique():
+    print(f'Testing year {year}')
     x_train = x[x['Year'] != year].drop(cols_to_exclude, axis=1)
     y_train = y.iloc[x_train.index]
     x_test = x[x['Year'] == year].drop(cols_to_exclude, axis=1)
@@ -70,4 +71,5 @@ for year in x['Year'].unique():
 results.loc[len(results.index)] = ['Avg'] + [results[col].mean() for col in results.columns if col != 'Test Year']
 results.loc[len(results.index)] = ['STD'] + [results[col].std() for col in results.columns if col != 'Test Year']
 print(results[['Test Year'] + [x for x in results.columns if x.endswith('Ll')]])
+print(results[['Test Year'] + [x for x in results.columns if x.endswith('Acc')]])
 results.to_csv('krieg_mm_results.csv', index=False)
--- krieg_plot_mm_results.py
+++ krieg_plot_mm_results.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from itertools import product
 import seaborn as sns
-sns.set(style="darkgrid", font_scale=1.8, rc={"lines.linewidth": 3})
+sns.set(style="darkgrid", font_scale=1.8, rc={"lines.linewidth": 4, "font.family":"Times New Roman"})
 
 df = pd.read_csv('krieg_mm_results.csv')
 df_new = pd.DataFrame(columns=['Test Year','Model','Accuracy','Log Loss'])
@@ -15,6 +15,8 @@ for y, m in product(years, models):
     df_new.loc[len(df_new.index)] = [y[:4], m, 
                df.loc[df['Test Year'] == y][m + ' ' + metrics[0]].values[0],
                df.loc[df['Test Year'] == y][m + ' ' + metrics[1]].values[0] ]
-
-#sns.relplot(data=df_new, kind='line', x='Test Year',y='Log Loss',hue='Model', style='Model', height=10, aspect=2)
+    
+sns.relplot(data=df_new, kind='line', x='Test Year',y='Accuracy', hue='Model', style='Model', height=8, aspect=2)
+sns.catplot(data=df_new, x='Model', y='Accuracy', kind='violin', ci='sd', height=8, aspect=1.5)
+sns.relplot(data=df_new, kind='line', x='Test Year',y='Log Loss',hue='Model', style='Model', height=8, aspect=2)
 sns.catplot(data=df_new, x='Model', y='Log Loss', kind='violin', ci='sd', height=8, aspect=1.5)
