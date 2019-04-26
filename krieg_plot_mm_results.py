451e299a270b59eb237cd32ea4c688a5685493be
sjkrieg <44177511+sjkrieg@users.noreply.github.com>
2019-04-26 15:33:40 -0400

    Add files via upload

--- krieg_plot_mm_results.py
+++ krieg_plot_mm_results.py
@@ -6,17 +6,22 @@ sns.set(style="darkgrid", font_scale=1.8, rc={"lines.linewidth": 4, "font.family
 df = pd.read_csv('krieg_mm_results.csv')
 df_new = pd.DataFrame(columns=['Test Year','Model','Accuracy','Log Loss'])
 
-models = ['NB','LR','XGB']
+models = ['NB','LR','XGB','Seed']
 metrics = ['Acc','Ll']
 rows_to_exclude = ['Avg','STD']
 years = [x for x in df['Test Year'].unique() if x not in rows_to_exclude]
 
 for y, m in product(years, models):
+    if m != 'Seed':
+        logloss = df.loc[df['Test Year'] == y][m + ' ' + metrics[1]].values[0]
+    else:
+        logloss = 1
     df_new.loc[len(df_new.index)] = [y[:4], m, 
                df.loc[df['Test Year'] == y][m + ' ' + metrics[0]].values[0],
-               df.loc[df['Test Year'] == y][m + ' ' + metrics[1]].values[0] ]
+               logloss ]
+        
     
-sns.relplot(data=df_new, kind='line', x='Test Year',y='Accuracy', hue='Model', style='Model', height=8, aspect=2)
+sns.relplot(data=df_new, kind='line', x='Test Year',y='Accuracy', hue='Model', style='Model', height=10, aspect=2)
 sns.catplot(data=df_new, x='Model', y='Accuracy', kind='violin', ci='sd', height=8, aspect=1.5)
-sns.relplot(data=df_new, kind='line', x='Test Year',y='Log Loss',hue='Model', style='Model', height=8, aspect=2)
-sns.catplot(data=df_new, x='Model', y='Log Loss', kind='violin', ci='sd', height=8, aspect=1.5)
+sns.relplot(data=df_new[df_new['Model'] != 'Seed'], kind='line', x='Test Year',y='Log Loss',hue='Model', style='Model', height=10, aspect=2)
+sns.catplot(data=df_new[df_new['Model'] != 'Seed'], x='Model', y='Log Loss', kind='violin', ci='sd', height=8, aspect=1.5)
