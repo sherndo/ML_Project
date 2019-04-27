1d5f132c18d388735c1223e682c34805eb60f383
Seth Herndon <sherndo7@gmail.com>
2019-04-27 16:49:01 -0400

    Fixed some other stuff for Grid search

--- algorithms_seth.py
+++ algorithms_seth.py
@@ -33,130 +33,133 @@ def trainXGBoost(data, classes, k_fold, scoring):
     avg_roc = []
     avg_loss = []
 
-    params = {
+    # params = {
     # "learning_rate"    : [0.01, 0.10, 0.30 ] ,
     # "max_depth"        : [3, 7, 10, 15],
     # "random_state": [7, 42, 145],
-    "min_child_weight" : [ 1, 3, 7 ],
-    "gamma"            : [ 0.0, 0.2 , 0.4 ],
-    "colsample_bytree" : [ 0.3, 0.4 , 0.7 ],
+    # "min_child_weight" : [ 1, 3, 7 ],
+    # "gamma"            : [ 0.0, 0.2 , 0.4 ],
+    # "colsample_bytree" : [ 0.3, 0.4 , 0.7 ],
     # "booster" : ['gbtree', 'gblinear', 'dart']
-    }
+    # }
 
     gbc = XGBClassifier(n_jobs=8, random_state=7, max_depth=3, booster='dart', learning_rate=0.1)
 
-    glf = GridSearchCV(gbc, params,cv=k_fold, scoring=scoring,refit='loss', return_train_score=True)
-    glf.fit(data,classes)
-    print(glf.best_params_)
-    results = glf.cv_results_
-    with open('/home/sherndo/Documents/Classes/MachineLearning/ML_Project/xgb_results.pkl', 'wb') as fp:
-        pickle.dump(results,fp)
-
-    # for key in params.keys():
-    plt.figure(figsize=(13, 13))
-    plt.title("GridSearchCV Evalution for Loss: XGBoost" ,
-            fontsize=16)
-
-    plt.xlabel('Evaluation Number')
-    plt.ylabel("Loss")
-
-    ax = plt.gca()
-    # ax.set_xlim(0, 402)
-    # ax.set_ylim(0.73, 1)
-
-    # Get the regular numpy array from the MaskedArray
-    X_axis = numpy.array(range(len(results['param_gamma'].data)), dtype=float)
-
-    # for scorer, color in zip(sorted(scoring), ['g', 'k']):
-    scorer='loss'
-    color='g'
-    sample = 'test'
-    style = '-'
-    # for sample, style in (('train', '--'), ('test', '-')):
-    sample_score_mean = results['mean_%s_%s' % (sample, scorer)]
-    sample_score_std = results['std_%s_%s' % (sample, scorer)]
-    sample_score_mean *= -1
-    sample_score_std *= -1
-    ax.fill_between(X_axis, sample_score_mean - sample_score_std,
-                    sample_score_mean + sample_score_std,
-                    alpha=0.1 if sample == 'test' else 0, color=color)
-    ax.plot(X_axis, sample_score_mean, style, color=color,
-            alpha=1 if sample == 'test' else 0.7,
-            label="%s (%s)" % (scorer, sample))
-
-    best_index = numpy.nonzero(results['rank_test_%s' % scorer] == 1)[0][0]
-    best_score = results['mean_test_%s' % scorer][best_index]
-
-    # Plot a dotted vertical line at the best score for that scorer marked by x
-    ax.plot([X_axis[best_index], ] * 2, [0, best_score],
-            linestyle='-.', color=color, marker='x', markeredgewidth=3, ms=8)
-
-    # Annotate the best score for that scorer
-    ax.annotate("%0.2f" % best_score,
-                (X_axis[best_index], best_score + 0.005))
-
-    plt.legend(loc="best")
-    plt.grid('off')
-    plt.savefig('/home/sherndo/Documents/Classes/MachineLearning/ML_Project/xg_param_search2_loss.png')
-
-    # glf = GridSearchCV(gbc, params,cv=k_fold, scoring=scoring,refit='accuracy', return_train_score=True)
+    # glf = GridSearchCV(gbc, params,cv=k_fold, scoring=scoring,refit='loss', return_train_score=True)
     # glf.fit(data,classes)
     # print(glf.best_params_)
     # results = glf.cv_results_
     # with open('/home/sherndo/Documents/Classes/MachineLearning/ML_Project/xgb_results.pkl', 'wb') as fp:
-        # pickle.dump(results,fp)
+    #     pickle.dump(results,fp)
 
-    # for key in params.keys():
-    plt.figure(figsize=(13, 13))
-    plt.title("GridSearchCV Evalution for Accuracy: XGBoost" ,
-            fontsize=16)
+    # # for key in params.keys():
+    # plt.figure(figsize=(13, 13))
+    # plt.title("GridSearchCV Evalution for Loss: XGBoost" ,
+    #         fontsize=16)
 
-    plt.xlabel('Evaluation Number')
-    plt.ylabel("Accuracy")
+    # plt.xlabel('Evaluation Number')
+    # plt.ylabel("Loss")
 
-    ax = plt.gca()
-    # ax.set_xlim(0, 402)
-    # ax.set_ylim(0.73, 1)
+    # ax = plt.gca()
+    # # ax.set_xlim(0, 402)
+    # # ax.set_ylim(0.73, 1)
 
-    # Get the regular numpy array from the MaskedArray
-    X_axis = numpy.array(range(len(results['param_gamma'].data)), dtype=float)
+    # # Get the regular numpy array from the MaskedArray
+    # X_axis = numpy.array(range(len(results['param_gamma'].data)), dtype=float)
 
-    # for scorer, color in zip(sorted(scoring), ['g', 'k']):
-    scorer='accuracy'
-    color='g'
-    sample = 'test'
-    style = '-'
-    # for sample, style in (('train', '--'), ('test', '-')):
-    sample_score_mean = results['mean_%s_%s' % (sample, scorer)]
-    sample_score_std = results['std_%s_%s' % (sample, scorer)]
-    ax.fill_between(X_axis, sample_score_mean - sample_score_std,
-                    sample_score_mean + sample_score_std,
-                    alpha=0.1 if sample == 'test' else 0, color=color)
-    ax.plot(X_axis, sample_score_mean, style, color=color,
-            alpha=1 if sample == 'test' else 0.7,
-            label="%s (%s)" % (scorer, sample))
+    # # for scorer, color in zip(sorted(scoring), ['g', 'k']):
+    # scorer='loss'
+    # color='g'
+    # sample = 'test'
+    # style = '-'
+    # # for sample, style in (('train', '--'), ('test', '-')):
+    # sample_score_mean = results['mean_%s_%s' % (sample, scorer)]
+    # sample_score_std = results['std_%s_%s' % (sample, scorer)]
+    # sample_score_mean *= -1
+    # sample_score_std *= -1
+    # ax.fill_between(X_axis, sample_score_mean - sample_score_std,
+    #                 sample_score_mean + sample_score_std,
+    #                 alpha=0.1 if sample == 'test' else 0, color=color)
+    # ax.plot(X_axis, sample_score_mean, style, color=color,
+    #         alpha=1 if sample == 'test' else 0.7,
+    #         label="%s (%s)" % (scorer, sample))
 
-    best_index = numpy.nonzero(results['rank_test_%s' % scorer] == 1)[0][0]
-    best_score = results['mean_test_%s' % scorer][best_index]
+    # best_index = numpy.nonzero(results['rank_test_%s' % scorer] == 1)[0][0]
+    # best_score = results['mean_test_%s' % scorer][best_index]
 
-    # Plot a dotted vertical line at the best score for that scorer marked by x
-    ax.plot([X_axis[best_index], ] * 2, [0, best_score],
-            linestyle='-.', color=color, marker='x', markeredgewidth=3, ms=8)
+    # # Plot a dotted vertical line at the best score for that scorer marked by x
+    # ax.plot([X_axis[best_index], ] * 2, [0, best_score],
+    #         linestyle='-.', color=color, marker='x', markeredgewidth=3, ms=8)
 
-    # Annotate the best score for that scorer
-    ax.annotate("%0.2f" % best_score,
-                (X_axis[best_index], best_score + 0.005))
+    # # Annotate the best score for that scorer
+    # ax.annotate("%0.2f" % best_score,
+    #             (X_axis[best_index], best_score + 0.005))
 
-    plt.legend(loc="best")
-    plt.grid('off')
-    plt.savefig('/home/sherndo/Documents/Classes/MachineLearning/ML_Project/xg_param_search2_accuracy.png')
-    
+    # plt.legend(loc="best")
+    # plt.grid('off')
+    # plt.savefig('/home/sherndo/Documents/Classes/MachineLearning/ML_Project/xg_param_search2_loss.png')
+
+    # # glf = GridSearchCV(gbc, params,cv=k_fold, scoring=scoring,refit='accuracy', return_train_score=True)
+    # # glf.fit(data,classes)
+    # # print(glf.best_params_)
+    # # results = glf.cv_results_
+    # # with open('/home/sherndo/Documents/Classes/MachineLearning/ML_Project/xgb_results.pkl', 'wb') as fp:
+    #     # pickle.dump(results,fp)
+
+    # # for key in params.keys():
+    # plt.figure(figsize=(13, 13))
+    # plt.title("GridSearchCV Evalution for Accuracy: XGBoost" ,
+    #         fontsize=16)
+
+    # plt.xlabel('Evaluation Number')
+    # plt.ylabel("Accuracy")
+
+    # ax = plt.gca()
+    # # ax.set_xlim(0, 402)
+    # # ax.set_ylim(0.73, 1)
+
+    # # Get the regular numpy array from the MaskedArray
+    # X_axis = numpy.array(range(len(results['param_gamma'].data)), dtype=float)
+
+    # # for scorer, color in zip(sorted(scoring), ['g', 'k']):
+    # scorer='accuracy'
+    # color='g'
+    # sample = 'test'
+    # style = '-'
+    # # for sample, style in (('train', '--'), ('test', '-')):
+    # sample_score_mean = results['mean_%s_%s' % (sample, scorer)]
+    # sample_score_std = results['std_%s_%s' % (sample, scorer)]
+    # ax.fill_between(X_axis, sample_score_mean - sample_score_std,
+    #                 sample_score_mean + sample_score_std,
+    #                 alpha=0.1 if sample == 'test' else 0, color=color)
+    # ax.plot(X_axis, sample_score_mean, style, color=color,
+    #         alpha=1 if sample == 'test' else 0.7,
+    #         label="%s (%s)" % (scorer, sample))
+
+    # best_index = numpy.nonzero(results['rank_test_%s' % scorer] == 1)[0][0]
+    # best_score = results['mean_test_%s' % scorer][best_index]
+
+    # # Plot a dotted vertical line at the best score for that scorer marked by x
+    # ax.plot([X_axis[best_index], ] * 2, [0, best_score],
+    #         linestyle='-.', color=color, marker='x', markeredgewidth=3, ms=8)
+
+    # # Annotate the best score for that scorer
+    # ax.annotate("%0.2f" % best_score,
+    #             (X_axis[best_index], best_score + 0.005))
+
+    # plt.legend(loc="best")
+    # plt.grid('off')
+    # plt.savefig('/home/sherndo/Documents/Classes/MachineLearning/ML_Project/xg_param_search2_accuracy.png')
+
     gbc_scores = cross_validate(gbc, data, classes, cv=k_fold, scoring=scoring, return_train_score=False, return_estimator=True)
 
-    # for i in range(len(gbc_scores['estimator'])):
-    #     gbc_i = gbc_scores['estimator'][i]
-    #     plot_importance(gbc_i, importance_type='cover')
-    #     plt.savefig('/home/sherndo/Documents/Classes/MachineLearning/ML_Project/xg_'+str(i)+'.png')
+    # gbc_mean = numpy.mean(gbc_scores['estimator'], axis=0)
+    # gbc_std = numpy.std(gbc_scores['estimator'], axis=0)
+
+    for i in range(len(gbc_scores['estimator'])):
+        # x = gbc_scores['estimator'][i].feature_importances_()
+        plot_importance(gbc_scores['estimator'][i], importance_type='cover')
+        plt.savefig('/home/sherndo/Documents/Classes/MachineLearning/ML_Project/xg_'+str(i)+'_features.png')
 
     return gbc_scores
 
@@ -167,13 +170,15 @@ def trainLogisticRegression(data, classes, k_fold, scoring):
     'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000],
     'tol': [0.001, 0.0001, 0.00001, 0.000001, 0.0000001],
     'solver': ['liblinear', 'lbfgs', 'saga'],
-    'max_iter': [100,1000,10000,100000]
+    'max_iter': [100,500,1000]
     }
+
+    {'C': 1000, 'solver': 'lbfgs', 'tol': 0.001}
 
     clf = GridSearchCV(lrc, params,cv=k_fold,scoring=scoring,refit='loss', return_train_score=True)
     clf.fit(data,classes)
     print(clf.best_params_)
-    results = glf.cv_results_
+    results = clf.cv_results_
     with open('/home/sherndo/Documents/Classes/MachineLearning/ML_Project/lr_results.pkl', 'wb') as fp:
         pickle.dump(results,fp)
 
